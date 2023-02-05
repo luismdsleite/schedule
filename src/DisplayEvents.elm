040f613762429cd55c78f5eddc02fc637d6193f7
luismdsleite <luismdsleite@gmail.com>
2023-02-05 02:10:46 +0000

    Expose DisplayEvent time-related constants

--- src/DisplayEvents.elm
+++ src/DisplayEvents.elm
@@ -1,4 +1,4 @@
-module DisplayEvents exposing (DisplayEvent(..), createDisplayEvents, startingHour, startingMinute, endingHour, endingMinute)
+module DisplayEvents exposing (DisplayEvent(..), createDisplayEvents, endingHour, endingMinute, startingHour, startingMinute)
 
 import Array exposing (Array)
 import Matrix exposing (..)
