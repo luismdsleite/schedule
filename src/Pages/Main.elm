3eb88330f73c36e863519b78b86d948ff02329d0
luismdsleite <luismdsleite@gmail.com>
2023-07-14 02:54:08 +0100

    Change Shared Model type

--- src/Pages/Main.elm
+++ src/Pages/Main.elm
@@ -1,6 +1,7 @@
 module Pages.Main exposing (Model, Msg, page)
 
 import Gen.Params.Main exposing (Params)
+import Html
 import Page
 import RenderMain.Model exposing (init)
 import RenderMain.Msg
@@ -10,18 +11,17 @@ import RenderMain.View exposing (view)
 import Request
 import Shared
 import View exposing (View)
-import Html
-
 
 
 page : Shared.Model -> Request.With Params -> Page.With Model Msg
 page shared req =
     Page.advanced
-        { init = init shared.data
+        { init = init shared
         , update = update
         , view = generateHtml "Main" view
         , subscriptions = subscriptions
         }
+
 
 generateHtml : String -> (Model -> Html.Html Msg) -> Model -> View Msg
 generateHtml title view model =
--- src/Shared.elm
+++ src/Shared.elm
@@ -7,11 +7,12 @@ module Shared exposing
     , update
     )
 
+import Dict
+import ElmSpa.Request exposing (Request)
+import Gen.Route
 import Json.Decode as Json
 import Request exposing (Request)
 import ScheduleObjects.Data exposing (Data)
-import Dict
-import Gen.Route
 
 
 type alias Flags =
@@ -19,7 +20,7 @@ type alias Flags =
 
 
 type alias Model =
-    {data : Data}
+    Data
 
 
 type Msg
@@ -28,14 +29,31 @@ type Msg
 
 init : Request -> Flags -> ( Model, Cmd Msg )
 init req _ =
-    ( {data = (Data Dict.empty Dict.empty Dict.empty Dict.empty)}, Request.pushRoute Gen.Route.Home_ req )
+    ( Data Dict.empty Dict.empty Dict.empty Dict.empty, Request.pushRoute Gen.Route.Home_ req )
 
 
 update : Request -> Msg -> Model -> ( Model, Cmd Msg )
 update req msg model =
     case msg of
         LoadedData data ->
-            ( {model | data = data}, Request.pushRoute Gen.Route.Main req )
+            ( data, Request.pushRoute Gen.Route.Main req )
+
+
+
+-- UpdateEventResult response ->
+--     case response of
+--         Ok ( id, updatedEvent ) ->
+--             let
+--                 newEvents =
+--                     model.events |> Dict.insert id updatedEvent
+--             in
+--             ( { model | events = newEvents }, Cmd.none )
+--         Err ( id, unmodifiedEvent ) ->
+--             let
+--                 newEvents =
+--                     model.events |> Dict.insert id unmodifiedEvent
+--             in
+--             ( { model | events = newEvents }, Cmd.none )
 
 
 subscriptions : Request -> Model -> Sub Msg
